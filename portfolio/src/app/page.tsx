bb7e251a64b97157d3452fc7f0c9edfd9f1adc1d
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-02-26 07:22:49 +0000

    Started a new layout, 5 containers slot into place on the page

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -4,49 +4,50 @@ import styles from "./page.module.css";
 import Header from "./components/Header/Header";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Projects from "./components/Projects/Projects";
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
 import Lenis from "lenis";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export default function Home() {
   const [isShrunk, setIsShrunk] = useState(false);
-
-  console.log("Header component rendered");
   console.log(isShrunk);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 10) {
+      if (window.scrollY > 20) {
         setIsShrunk(true);
       } else {
         setIsShrunk(false);
       }
     };
-
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   useEffect(() => {
-    // Initialize Lenis
     const lenis = new Lenis({
       autoRaf: true,
     });
 
-    // // Listen for the scroll event and log the event data
     // lenis.on("scroll", (e) => {
     //   console.log(e);
     // });
   }, []);
 
   return (
-    <div className={styles.page}>
-      <Header isShrunk={isShrunk} />
-      <AboutMe />
-      <Projects />
+    <div className="main">
+      <div className="div1">Hello</div>
+
+      <div className="div2">About me</div>
+      <div className="div3">Projects</div>
+      <div className="div4">Skills</div>
+      <div className="div5">Contact</div>
     </div>
+
+    // <div className={styles.page}>
+    //   <Header isShrunk={isShrunk} />
+    //   <div className={styles.wrapper}>
+    //     <AboutMe />
+    //     <Projects />
+    //   </div>
+    // </div>
   );
 }
